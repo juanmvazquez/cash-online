56e5e1d170de0347c5509427bb6b7b432d2b118c
JuanMVazquez <jvazquez@mobydigital.com>
2023-04-21 15:11:37 -0300

    initial commit

---
+++ src/main/java/com/challengecashonline/cashonline/CashOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.challengecashonline.cashonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashOnlineApplication.class, args);
+	}
+
+}
