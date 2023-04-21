89c637f19f292cfe80f4e0954958887ac01591e5
JuanMVazquez <jvazquez@mobydigital.com>
2023-04-21 15:09:56 -0300

    Se agrego el schema de las entidades y los insert

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (first_name, last_name, email) VALUES ('Pepe', 'Argento', 'pepe@example.com');
+INSERT INTO users (first_name, last_name, email) VALUES ('Pepe', 'Argento2', 'pepe2@example.com');
+INSERT INTO users (first_name, last_name, email) VALUES ('Coki', 'Argento', 'coki@example.com');
+
+INSERT INTO loans (total, user_id) VALUES (1500.00, 1);
+INSERT INTO loans (total, user_id) VALUES (2500.00, 1);
+INSERT INTO loans (total, user_id) VALUES (3500.00, 1);
+INSERT INTO loans (total, user_id) VALUES (2500.00, 2);
+INSERT INTO loans (total, user_id) VALUES (1500.00, 2);
+INSERT INTO loans (total, user_id) VALUES (1500.00, 3);
+INSERT INTO loans (total, user_id) VALUES (1500.00, 3);
